7b89e8560bc66c7758cd2e930ca789a92d2d6793
Vlad-Skliar <vlad.skliar@gmail.com>
2015-10-13 20:39:42 +0300

    Added str for Info models

--- apps/info/models.py
+++ apps/info/models.py
@@ -9,3 +9,6 @@ class Info(models.Model):
     birthdate = models.DateField('Date of birth')
     bio = models.CharField(max_length=255)
     contacts = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
