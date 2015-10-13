3427e73d0a5be73612bd80d772876455b5d96fc6
VladSkliar <vlad.skliar@gmail.com>
2015-10-13 19:15:10 +0300

    Added test view

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from apps.testapp import views
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.test_view, name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
